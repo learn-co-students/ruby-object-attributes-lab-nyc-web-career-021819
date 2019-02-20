4bd40725ad0196c1004af960e0fcb0762ecd981c
Jessy Hu <jesshuy12@gmail.com>
2019-02-20 17:20:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog 
+
+  def name
+    @name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+end
+
+class Dog
+  
+  def breed 
+    @breed
+  end
+  
+  def breed=(breed)
+    @breed = breed
+  end
+end
+
+
+
+    
