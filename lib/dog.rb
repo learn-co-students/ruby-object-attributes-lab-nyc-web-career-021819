f492bce49563905b86890a7d4bdb64c9528d0d7d
Lisa Scott <lisarenesoctt@gmail.com>
2019-04-23 11:20:18 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,24 @@
+
 class Dog
-  def initialize
+  def initalize
 
   end
 
   def name
     @name
   end
-  def name=(name)
-    @name = name
+
+  def name=(new_name)
+    @name = new_name
   end
+
   def breed
     @breed
   end
-  def breed=(breed)
-    @breed = breed
+
+  def breed=(new_breed)
+    @breed = new_breed
   end 
-
-
-
-
 
 
 
--- lib/person.rb
+++ lib/person.rb
@@ -1,22 +1,20 @@
-class Person
-  def initialize
 
-  end
+class Person
 
   def name
     @name
   end
-  def name=(name)
-    @name = name
+
+  def name=(new_name)
+    @name = new_name
   end
+
   def job
     @job
   end
-  def job=(job)
-    @job = job
-  end
 
-
-
+  def job=(new_job)
+    @job = new_job
+  end 
 
 end
